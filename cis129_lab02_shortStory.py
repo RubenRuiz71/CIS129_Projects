fce237a44f5bb627c25f1ce32703410073eddf98
RubenRuiz71 <rruiz71@mail.pima.edu>
2024-06-27 19:22:18 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,2 +1,20 @@
-print('Hello,World!')
+# In this code i will be explaining the narrative of the project 
+
+# the program will ask their own developer who is he talking to 
+
+
+print("whats  your name  ?")
+
+input ("my name is PROTOMAR LEA")
+
+print("what does PROTOMAR LEA stands for ?")
+
+input("Program To Make Ruben's Life Easier")
+
+print("who is your creator")
+input("Ruben Ruiz")
+
+
+
+
 
